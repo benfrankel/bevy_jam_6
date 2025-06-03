17436971e1f7c3e4dd72c800ea90b14fd791fc47
Ben Frankel <ben.frankel7@gmail.com>
2025-06-02 22:15:52 -0700

    Make missile use generic damage event

--- src/game/combat/damage.rs
+++ src/game/combat/damage.rs
@@ -21,8 +21,25 @@ pub struct OnDamage(pub f32);
 impl Configure for OnDamage {
     fn configure(app: &mut App) {
         app.register_type::<Self>();
+        app.add_observer(deal_damage_on_collision);
         app.add_observer(reduce_health_on_damage);
     }
+}
+
+fn deal_damage_on_collision(
+    trigger: Trigger<OnCollisionStart>,
+    mut commands: Commands,
+    damage_query: Query<&Damage>,
+    health_query: Query<(), With<Health>>,
+) {
+    let hitbox = r!(trigger.get_target());
+    let damage = rq!(damage_query.get(hitbox));
+
+    let hurtbox = trigger.collider;
+    rq!(health_query.contains(hurtbox));
+
+    commands.entity(hitbox).try_despawn();
+    commands.entity(hurtbox).trigger(OnDamage(damage.0));
 }
 
 fn reduce_health_on_damage(trigger: Trigger<OnDamage>, mut health_query: Query<&mut Health>) {
--- src/game/missile.rs
+++ src/game/missile.rs
@@ -1,9 +1,6 @@
 use crate::game::GameLayer;
 use crate::game::combat::damage::Damage;
 use crate::game::combat::faction::Faction;
-use crate::game::combat::health::Health;
-use crate::game::module::ModuleAction;
-use crate::game::module::OnModuleAction;
 use crate::prelude::*;
 
 pub(super) fn plugin(app: &mut App) {
@@ -15,48 +12,16 @@ pub fn missile(missile_assets: &MissileAssets, faction: Faction, damage: f32) ->
         Name::new("Missile"),
         IsMissile,
         Sprite::from_image(missile_assets.image.clone()),
+        Damage(damage),
+        faction,
         RigidBody::Dynamic,
         // TODO: Increase initial velocity, taking initial rotation into account.
         LinearVelocity::ZERO,
         MaxLinearSpeed(150.0),
         Collider::capsule(3.0, 7.0),
-        CollisionEventsEnabled,
         CollisionLayers::new(GameLayer::Default, faction.opponent().layer()),
-        faction,
-        Patch(|entity| {
-            entity.observe(hit_ship);
-        }),
-        Damage(damage),
+        CollisionEventsEnabled,
     )
-}
-
-fn hit_ship(
-    trigger: Trigger<OnCollisionStart>,
-    mut commands: Commands,
-    damage_query: Query<&Damage>,
-    mut health_query: Query<&mut Health>,
-) {
-    // Despawn the missile.
-    let missile = r!(trigger.get_target());
-    commands.entity(missile).try_despawn();
-
-    // Apply damage to the target ship.
-    let ship = trigger.collider;
-    if let Ok(damage) = damage_query.get(missile) {
-        let mut target_health = r!(health_query.get_mut(ship));
-        target_health.current -= damage.0;
-
-        // Detect ship death.
-        if target_health.current <= f32::EPSILON {
-            commands.entity(trigger.collider).try_despawn();
-            return;
-        }
-    }
-
-    // Fire a missile from the player ship.
-    commands
-        .entity(ship)
-        .trigger(OnModuleAction(ModuleAction::Missile));
 }
 
 #[derive(AssetCollection, Resource, Reflect, Default, Debug)]
