318e0814336d54332b1bc66744fb8109a0c99706
jayclees <jake.lee.web@gmail.com>
2025-06-04 20:24:51 -0700

    Match settings to mockup

--- src/menu/settings.rs
+++ src/menu/settings.rs
@@ -2,9 +2,11 @@ use bevy_simple_prefs::Prefs;
 use bevy_simple_prefs::PrefsPlugin;
 
 use crate::core::audio::AudioSettings;
+use crate::game::level::LevelAssets;
 use crate::menu::Menu;
 use crate::menu::MenuRoot;
 use crate::prelude::*;
+use crate::theme::color::ThemeConfig;
 
 pub(super) fn plugin(app: &mut App) {
     app.add_systems(StateFlush, Menu::Settings.on_enter(spawn_settings_menu));
@@ -17,14 +19,33 @@ pub(super) fn plugin(app: &mut App) {
     )>();
 }
 
-fn spawn_settings_menu(mut commands: Commands, menu_root: Res<MenuRoot>) {
-    commands
-        .entity(menu_root.ui)
-        .with_child(widget::body(children![
-            widget::header("[b]Settings"),
-            grid(),
-            widget::row_of_buttons(children![widget::wide_button("Back", go_back)]),
-        ]));
+fn spawn_settings_menu(
+    mut commands: Commands,
+    menu_root: Res<MenuRoot>,
+    level_assets: Res<LevelAssets>,
+    theme_config: ConfigRef<ThemeConfig>,
+) {
+    let theme_config = r!(theme_config.get());
+    commands.spawn((
+        Name::new("Background"),
+        Sprite::from_image(level_assets.bg_level0.clone()),
+        Transform::from_xyz(0.0, 0.0, -2.0),
+        DespawnOnExitState::<Menu>::Recursive,
+    ));
+    commands.entity(menu_root.ui).with_child((
+        widget::overlay(theme_config),
+        children![
+            Name::new("SettingsPopup"),
+            (
+                widget::popup_window(Vw(90.), Vh(90.), theme_config, None, None),
+                children![
+                    widget::header("[b]Settings"),
+                    grid(),
+                    widget::row_of_buttons(children![widget::wide_button("Back", go_back)]),
+                ],
+            ),
+        ],
+    ));
 }
 
 fn go_back(_: Trigger<Pointer<Click>>, mut menu: ResMut<NextStateStack<Menu>>) {
