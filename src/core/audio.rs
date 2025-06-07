4fb7117c508311a7dca029dc7ca3b5e40ae6e05f
Ben Frankel <ben.frankel7@gmail.com>
2025-06-07 15:22:44 -0700

    Balance audio some more

--- src/core/audio.rs
+++ src/core/audio.rs
@@ -22,7 +22,7 @@ impl Default for AudioSettings {
         Self {
             master_volume: 0.5,
             music_volume: 0.5,
-            sfx_volume: 0.5,
+            sfx_volume: 0.7,
             ui_volume: 0.5,
         }
     }
